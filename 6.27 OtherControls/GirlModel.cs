0a06984c4e509c8b88697dde66c4aa136e038b63
martin <martin@V-DOXIE-W10>
2016-06-27 17:36:50 +0800

    Add project files.

---
+++ 6.27 OtherControls/GirlModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._27_OtherControls
+{
+    public class GirlModel
+    {
+        public String Name { get; set; }
+        public String Image { get; set; }
+    }
+
+    public class GirlManager {
+        public static ObservableCollection<GirlModel> GetAllGirls() {
+            ObservableCollection<GirlModel> models = new ObservableCollection<GirlModel> {
+                new GirlModel { Name = "苍井空" ,Image="/Assets/meinv01.png"},
+                new GirlModel { Name = "美嘉子" ,Image="/Assets/meinv02.png"},
+                new GirlModel { Name = "小泽" ,Image="/Assets/meinv03.jpg"},
+            };
+            return models;
+        } 
+    }
+}
---
+++ 6.27 OtherControls/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _6._27_OtherControls
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public ObservableCollection<GirlModel> models { set; get; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //models = GirlManager.GetAllGirls();
+
+            //MyFileView.ItemsSource = models;
+        }
+
+        private void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
+        {
+            Debug.WriteLine(sender);
+        }
+    }
+}
